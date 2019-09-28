caeeb9e5d6cfe109bb10a950de059e94aa418849
Antoine Simon <antoine.simon.epitech@gmail.com>
2019-09-28 15:27:09 +0100

    start

---
+++ Demo-Babylon-Test/Demo-Babylon-Test/MainNavigationViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainNavigationViewController.swift
+//  Demo-Babylon-Test
+//
+//  Created by Antoine Simon on 28/09/2019.
+//  Copyright © 2019 antoine simon. All rights reserved.
+//
+
+import UIKit
+
+class MainNavigationViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
