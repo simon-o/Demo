091f5fdf81d70c3285d1ef28d1db72dfe2fd1c52
Antoine Simon <antoine.simon@epitech.eu>
2019-09-28 14:57:59 +0100

    start project

---
+++ Demo-List-Babylon/Demo-List-BabylonTests/Demo_List_BabylonTests.swift
@@ -0,0 +1,34 @@
+//
+//  Demo_List_BabylonTests.swift
+//  Demo-List-BabylonTests
+//
+//  Created by Antoine Simon on 28/09/2019.
+//  Copyright © 2019 antoine simon. All rights reserved.
+//
+
+import XCTest
+@testable import Demo_List_Babylon
+
+class Demo_List_BabylonTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
