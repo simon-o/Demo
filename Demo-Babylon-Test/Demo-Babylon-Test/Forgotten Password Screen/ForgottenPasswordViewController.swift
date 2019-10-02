bc7c041d7a6d966f686ee9e68c67984580f1fa13
Antoine Simon <antoine.simon.epitech@gmail.com>
2019-10-02 21:09:58 +0100

    Can close keyboard on drag on ForgottenPasswordVC

--- Demo-Babylon-Test/Demo-Babylon-Test/Forgotten Password Screen/ForgottenPasswordViewController.swift
+++ Demo-Babylon-Test/Demo-Babylon-Test/Forgotten Password Screen/ForgottenPasswordViewController.swift
@@ -37,8 +37,28 @@ final class ForgottenPasswordViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name:UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name:UIResponder.keyboardWillHideNotification, object: nil)
+        
         presenter.attachView(self)
         presenter.viewDidLoad()
+    }
+    
+    @objc func keyboardWillShow(notification:NSNotification){
+
+        let userInfo = notification.userInfo!
+        var keyboardFrame:CGRect = (userInfo[UIResponder.keyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
+        keyboardFrame = self.view.convert(keyboardFrame, from: nil)
+
+        var contentInset:UIEdgeInsets = self.scrollView.contentInset
+        contentInset.bottom = keyboardFrame.size.height
+        scrollView.contentInset = contentInset
+    }
+
+    @objc func keyboardWillHide(notification:NSNotification){
+
+        let contentInset:UIEdgeInsets = UIEdgeInsets.zero
+        scrollView.contentInset = contentInset
     }
     
     @IBAction func sendButtonClicked(_ sender: Any) {
